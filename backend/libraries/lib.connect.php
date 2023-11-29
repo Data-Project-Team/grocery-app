0d87c76989ad4e69b2be52c22506773b02f60649
ryabubaker <ryabubaker@gmail.com>
2023-11-29 18:51:48 +0200

    Upload the RestAPI

---
+++ backend/libraries/lib.connect.php
@@ -0,0 +1,16 @@
+<?php
+/**
+###########################################
+### This file contains database  server  ##
+###########################################
+**/
+
+$engine	  = "mysqli";
+$port     = "3306";
+$server   = "192.168.15.126";
+$username = "root";
+$password = ""; 
+$database = "arex";
+
+
+?>
