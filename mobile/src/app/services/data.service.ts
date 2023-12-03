4aa5eb5c4aac4b07d187746dd0bccca59cbb278b
ryabubaker <ryabubaker@gmail.com>
2023-12-03 23:23:50 +0200

    add data and resolver service

---
+++ mobile/src/app/services/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private data:string[] = [];
+
+  constructor() { }
+  
+  setData(id:any, data:any){
+    this.data[id]=data;
+  }
+  getData(id:any){
+    return this.data[id];
+  }
+
+}
