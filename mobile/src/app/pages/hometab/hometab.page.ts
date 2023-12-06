45db90bdbda07b469716f12e2e05c4625c574142
Abdelrhmankhalifa1 <103604707+Abdelrhmankhalifa1@users.noreply.github.com>
2023-12-06 20:20:49 +0200

    initial home page

--- mobile/src/app/pages/hometab/hometab.page.ts
+++ mobile/src/app/pages/hometab/hometab.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { register } from 'swiper/element/bundle';
+import { DataService } from '../../services/data.service';
+import { ApiService } from 'src/app/services/api.service';
+import { CartService } from '../../services/cart.service';
 
+register();
 @Component({
-  selector: 'app-hometab',
-  templateUrl: './hometab.page.html',
-  styleUrls: ['./hometab.page.scss'],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HometabPage implements OnInit {
+export class HomePage implements OnInit {
+  categories = [
+    //grocery categories and their pictures 
+  ];
 
-  constructor() { }
+  trending: any = [];
+  onSale: any = [];
+  isLoading:boolean = false;
+  liked!:boolean;
+  
+  constructor(private router: Router, public data:DataService, public api: ApiService)  {
+    
+   }
 
-  ngOnInit() {
-  }
-
+   /// fetching respose 
+  
+ 
 }
