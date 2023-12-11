ac6c545cce273ce8c4bc2be6d17de6e7fc674fd1
mennatullah77 <menna.ashraf99830@gmail.com>
2023-12-11 22:01:15 +0200

    Add search component on the homepagetab

--- mobile/src/app/pages/hometab/hometab.page.ts
+++ mobile/src/app/pages/hometab/hometab.page.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-hometab',
   templateUrl: './hometab.page.html',
   styleUrls: ['./hometab.page.scss'],
 })
 export class HometabPage implements OnInit {
-
-  constructor() { }
+  categories = [
+    { name: 'Fashion', icon: 'fashion.jpg' },
+    { name: 'Accessories', icon: 'favicon.png' },
+    { name: 'Electronics', icon: 'electronics.png' },
+    { name: 'Beauty and Personal Care', icon: 'favicon.png' },
+    { name: 'Sports and Outdoors', icon: 'sports.png' },
+    { name: 'Health and Wellness', icon: 'favicon.png' },
+    
+  ];
+  trending: any = [];
+  onSale: any = [];
+  isLoading:boolean = false;
+  liked!:boolean;
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
