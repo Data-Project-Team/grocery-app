08269b37ea812f83685732ebfa9f675e86606ccf
ryabubaker <ryabubaker@gmail.com>
2023-12-14 01:09:06 +0200

    add details page frontend

--- mobile/src/app/pages/productspage/productspage.module.ts
+++ mobile/src/app/pages/productspage/productspage.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { ProductspagePageRoutingModule } from './productspage-routing.module';
 
 import { ProductspagePage } from './productspage.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     ProductspagePageRoutingModule
   ],
   declarations: [ProductspagePage]
