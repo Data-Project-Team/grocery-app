260d98337b32442a39640b45c97765e040a8f94d
Nancy <nancyhossam2002@gmail.com>
2023-12-16 00:50:53 +0200

    add cart button component and html file

---
+++ mobile/src/app/components/add-cart-btn/add-cart-btn.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cart-btn',
+  templateUrl: './add-cart-btn.component.html',
+  styleUrls: ['./add-cart-btn.component.scss'],
+})
+export class AddCartBtnComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
