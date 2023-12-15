dffa8d8d5c9149f18e4a516964019c5356c96492
Nancy <nancyhossam2002@gmail.com>
2023-12-16 00:37:46 +0200

    cart service ts file initialization

--- mobile/src/app/services/cart.service.ts
+++ mobile/src/app/services/cart.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
 
-  constructor() { }
+export class CartService {
+  totalPrice: number =  0;
+  constructor(public data: DataService) {}
 }
