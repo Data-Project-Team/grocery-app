861063aefd13873585965e7176f1dbcdf5b13bc9
Mennatullah Ashraf Mohasseb <62481023+Mennatullah77@users.noreply.github.com>
2023-12-20 01:55:25 +0200

    Modifying the routing for the accounttab

--- mobile/src/app/pages/pages-routing.module.ts
+++ mobile/src/app/pages/pages-routing.module.ts
@@ -33,6 +33,10 @@ const routes: Routes = [
         loadChildren: () => import('./hometab/hometab.module').then( m => m.HometabPageModule)
       },
       {
+        path: 'accounttab',
+        loadChildren: () => import('./accounttab/accounttab.module').then( m => m.AccounttabPageModule)
+      },
+      {
         path: 'categorytab',
         children: [{
           path: '',
