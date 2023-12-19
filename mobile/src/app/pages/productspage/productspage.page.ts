7d6d5b9fe5fd4f6afded151089e8e76ce63be3e9
Abdelrhmankhalifa1 <boodykh2002@gmail.com>
2023-12-19 12:43:29 +0200

    Update productspage.page.ts

--- mobile/src/app/pages/productspage/productspage.page.ts
+++ mobile/src/app/pages/productspage/productspage.page.ts
@@ -14,6 +14,7 @@ export class ProductspagePage  {
 
   products:any = [];
   params: string = '';
+  ctgname: string = '';
   isLoading:boolean = false;
   currentSort: string = '';
   currentSortLabel: string = 'Sort'; 
@@ -24,24 +25,109 @@ export class ProductspagePage  {
     { label: 'Price: lowest to high', value: 'ascending' },
     { label: 'Price: highest to low', value: 'descending' }
   ];
+
+  brands = [
+    { name: 'Grocera' },
+    { name: 'Almarai' },
+    { name: 'Juhayna' },
+    { name: 'Heinz' }
+  ];
+  origins = [
+    { name: 'Egypt' },
+    { name: 'USA' },
+    { name: 'France' },
+    { name: 'Saudi Arabia' },
+    { name: 'Emirates' },
+  ];
+  priceRange = { lower: 0, upper: 100 }; // Initialize with default values
+  selectedBrand: string = '';
+  selectedOrigin: string = '';
+  selectedSort: string = '';
+ 
+  
   
   constructor(public api: ApiService, public data: DataService, private router: Router) {
     
-
+    const catinfo: any = this.data.getData("i");
+    
+    this.params = catinfo.id;
+    this.ctgname= catinfo.name;
     
    }
 
    ionViewDidEnter(){
+    this.fetchProductsByParams();
+    console.log(this.params);
   
   }
 
+  fetchProductsByParams(){
+    const action = "fetchproducts";
+    const usrCode = localStorage.getItem("usr_code");
+    const data = '&categoryId=' + this.params + '&usrCode=' + usrCode;
+
+    this.products=[];
+    this.api.getData(action, data).then(
+      (response: any) => {
+        if(response.msg === 'success'){
+          this.products = response.data;
+          this.isLoading = true;
+        }else{
+          this.products = [];
+        }
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+ 
 
 
  
   openModal() {
     this.modal.present(); // Show the modal
   }
+  onPriceRangeChange(event: any) {
+    this.priceRange = event.detail.value;
+  }
+  applyFilters() {
+    const filters = {
+      brand: this.selectedBrand,
+      priceRange: this.priceRange,
+      origin: this.selectedOrigin,
+      sort: this.selectedSort
+    };
+    const action = "filter";
+    const usrCode = localStorage.getItem("usr_code");
+    const data = '&usrCode=' + usrCode +'&categoryId=' + this.params + '&brand=\''  + filters.brand + '\'&min=' + filters.priceRange.lower + '&max=' + filters.priceRange.upper + '&origin=\'' + filters.origin + '\'&sortby=' +filters.sort;
+    console.log(data);
+    this.api.getData(action, data).then(
+      (response: any) => {
+        console.log(response);
+        if (response.msg === 'success') {
+          this.products = response.data;
+          this.isLoading = true;
+          this.modal.dismiss(); 
+        } else {
+          this.products = [];
+        }
+      },
+      (error) => {
+        console.error('Error applying filters:', error);
+      }
+    );
+    
+  }
+  resetFilters() {
+    this.selectedBrand = '';
+    this.priceRange = { lower: 0, upper: 100 }; // Reset to default values
+    this.selectedOrigin = '';
+    this.selectedSort='';
+  }
+   
 
 
-
+  
+  
 }
